c32d84dbd0d6dd10329a00756c0b94f6b3d8e885
ocsiker <qthang89vn@gmail.com>
2025-02-13 11:16:32 +0700

    feat:change brightness to 0.9

--- defaultLcd.sh
+++ defaultLcd.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-xrandr --output DP-2 --primary --mode 1280x1024 --rate 75.02 --output HDMI-1 --mode 1280x1024 --rate 75.02 --left-of DP-2
+xrandr --output DP-2 --primary --mode 1280x1024 --rate 75.02 --brightness 0.9 --output HDMI-1 --mode 1280x1024 --rate 75.02 --brightness 0.9 --left-of DP-2
